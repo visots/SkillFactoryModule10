cc960c3b7d1ad4de8ca5a352ead599ccfae388d5
Alexey <visotskiy.alexey@gmail.com>
2023-03-01 22:03:56 +0700

    Задание 10-4-4

---
+++ Ex10_4_4/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ex10_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var account = new Account();
+            var user = new User();
+
+            IUpdater<Account> updater = new UserService();
+
+            var userService = new UserService();
+            userService.Update(user);
+
+        }
+    }
+
+    public class User
+    {
+
+    }
+
+    public class Account : User
+    {
+
+    }
+
+    public interface IUpdater <in T>
+    {
+        void Update(T entity);
+    }
+
+    public class UserService : IUpdater<User> 
+    {
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
