a3ca257d8938119f76619be7b4d356b3de8af97b
Alexey <visotskiy.alexey@gmail.com>
2023-03-01 22:33:39 +0700

    Итогове задание 1

---
+++ FinalTask10_5/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FinalTask10_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Calculator calculator = new Calculator();
+
+            while (true)
+            {
+                Console.WriteLine("Введите значение Х");
+                int x = ReadValue();
+                Console.WriteLine("Введите значение Y");
+                int y = ReadValue();
+
+                Console.WriteLine("Результат: " + calculator.Sum(x, y));
+                Console.ReadKey();
+            }
+
+        }
+
+        static int ReadValue()
+        {
+            int value = 0;
+
+            try
+            {
+                value = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка, введите числовое значение");
+            }
+
+            return value;
+        }
+
+    }
+
+    public interface ISum
+    {
+        public int Sum(int x, int y);
+    }
+
+    public class Calculator : ISum
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
